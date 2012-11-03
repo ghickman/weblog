b19f16862dd4979113396d5b4234553f91a24649
George Hickman <george@ghickman.co.uk>
2012-11-03 16:00:09 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/tv', methods=('GET',))
+def tv():
+    with open(os.environ['LOG_FILE'], 'r') as f:
+        lines = [line.strip() for line in f.readlines()[-2:]]
+        lines.reverse()
+        return render_template('home.html', lines=lines)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
